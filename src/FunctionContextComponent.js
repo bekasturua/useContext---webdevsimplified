6542d7714713856fcc44abce50aea7ca39c17368
Beka Sturua <beqasturua3@gmail.com>
2022-07-17 23:34:46 +0400

    consider useEffect

--- src/FunctionContextComponent.js
+++ src/FunctionContextComponent.js
@@ -1,4 +1,23 @@
 import React, { useContext } from "react";
+import { ThemeContext } from "./App";
+import { useTheme, useThemeUpdate } from "./ThemeContext";
+
 export default function FunctionContextComponent() {
-  return <div>Function Theme</div>;
+  const darkTheme = useTheme();
+  const toggleTheme = useThemeUpdate();
+  const themeStyles = {
+    backgoundColor: darkTheme ? "#333" : "#CCC",
+    color: darkTheme ? "#CCC" : "#333",
+    padding: "2rem",
+    margin: "2rem",
+  };
+  return (
+    <>
+      <button onClick={toggleTheme}>Toggle Theme</button>
+      <div className="div" style={themeStyles}>
+        Function Theme
+      </div>
+      ;
+    </>
+  );
 }
